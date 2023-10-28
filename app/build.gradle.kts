9f7d8a3cace6b62425488e877ec3874cadf6f038
none <none@>
2023-10-28 11:32:35 -0300

    changed min sdk to 19 (KitKat)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.droidev.wwm"
-        minSdk = 24
+        minSdk = 19
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
